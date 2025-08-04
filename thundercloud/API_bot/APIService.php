7399a516861a1bf324bcdb2d76c37b396327423b
soyMatuDev <juanjesusgarciamaturana@gmail.com>
2025-08-04 17:30:24 -0600

    api bot

--- thundercloud/API_bot/APIService.php
+++ thundercloud/API_bot/APIService.php
@@ -13,34 +13,52 @@ require_once(__DIR__ . '/../ThunderLog/ThunderLog.php');
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/Bot.php');
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
-$dotenv->load();
+class API_BOT {
 
-if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-    http_response_code(405);
-    header('Content-Type: application/json');
-    ReturnEvent::returnResponse(1, "Método no permitido", "Solo se permiten solicitudes POST");
-    exit;
+    function API () {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+        $dotenv->load();
+    
+        if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+            http_response_code(405);
+            header('Content-Type: application/json');
+            ReturnEvent::returnResponse(1, "Método no permitido", "Solo se permiten solicitudes POST");
+            exit;
+        }
+    
+        $body = json_decode(file_get_contents('php://input'), true);
+    
+        // Validar que venga algo válido de Telegram
+        $chatId = $body['message']['chat']['id'] ?? null;
+        $text = $body['message']['text'] ?? null;
+    
+        if (!$chatId || !$text) {
+            // Silenciosamente ignorar
+            header('Content-Type: application/json');
+            ReturnEvent::returnResponse(1, "Datos incompletos", "El mensaje no contiene chatId o texto");
+            exit;
+        }
+    
+        // Configuración del bot de Telegram 
+        // Logica para escoger el token del bot
+        $BOT_TOKEN = $_ENV['TELEGRAM_BOT_TOKEN'] ?? null;
+    
+        // Crear instancia del bot
+        $bot = new Bot($BOT_TOKEN, $chatId, $text);
+        // Enviar respuesta
+        $bot->bot_response();
+    
+    }
 }
 
-$body = json_decode(file_get_contents('php://input'), true);
-
-// Validar que venga algo válido de Telegram
-$chatId = $body['message']['chat']['id'] ?? null;
-$text = $body['message']['text'] ?? null;
-
-if (!$chatId || !$text) {
-    // Silenciosamente ignorar
-    header('Content-Type: application/json');
-    ReturnEvent::returnResponse(1, "Datos incompletos", "El mensaje no contiene chatId o texto");
-    exit;
+function main()
+{
+    // Leer el cuerpo de la solicitud HTTP
+    $contenido = file_get_contents("php://input");
+    $data = json_decode($contenido, true);
+    $componenteService = new API_BOT($data['uu']);
+    $func = $data['function'];
+    $componenteService->$func($data['uu'], $data['cc'], ...$data['args']);
 }
 
-// Configuración del bot de Telegram 
-// Logica para escoger el token del bot
-$BOT_TOKEN = $_ENV['TELEGRAM_BOT_TOKEN'] ?? null;
-
-// Crear instancia del bot
-$bot = new Bot($BOT_TOKEN, $chatId, $text);
-// Enviar respuesta
-$bot->bot_response();
+main();
