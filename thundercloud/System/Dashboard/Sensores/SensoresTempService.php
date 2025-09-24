11660fbd9650fef92c89e9307fb25d471635ba02
juan <jmaturana@pcz.com.mx>
2025-09-24 09:55:32 -0600

    fix: Temperatura mas actual sin registros

--- thundercloud/System/Dashboard/Sensores/SensoresTempService.php
+++ thundercloud/System/Dashboard/Sensores/SensoresTempService.php
@@ -73,13 +73,15 @@ class SensoresTempService
           return null;
         }
   
-        $query = "SELECT a.clave, a.nombre, a.serie, a.modelo, b.descri as unidad, c.nombre as zona, a.alias, a.materia, d.fecha_hora, d.dato_1 as temp, d.dato_2 as hum 
-        from ma_equipo a, ma_unidad b, de_zona c, ma_regzoro d
-        where cve_unidad = 'TEM'
-        and a.cve_zona = c.clave
-        and a.cve_unidad = b.clave
-        and a.clave = d.cve_equipo
-        and fecha_hora = (SELECT MAX(fecha_hora) FROM ma_regzoro WHERE cve_equipo = a.clave)";
+        $query = "SELECT a.clave, a.nombre, a.serie, a.modelo, b.descri as unidad, c.nombre as zona, a.alias, a.materia, d.fecha_hora, 
+        case when d.fecha_hora >= NOW() - INTERVAL '2 hour' then d.dato_1 else '0.0' end as temp, 
+        case when d.fecha_hora >= NOW() - INTERVAL '2 hour' then d.dato_2 else '0.0' end as hum 
+                from ma_equipo a, ma_unidad b, de_zona c, ma_regzoro d
+                where cve_unidad = 'TEM'
+                and a.cve_zona = c.clave
+                and a.cve_unidad = b.clave
+                and a.clave = d.cve_equipo
+                and fecha_hora = (SELECT MAX(fecha_hora) FROM ma_regzoro WHERE cve_equipo = a.clave)";
         $query .= $nombre ? " and a.nombre = '$nombre'" : "";
         $stmt = new Statement($this->conn, (null));
         $res = $stmt->prepareStatement($query);
@@ -96,8 +98,8 @@ class SensoresTempService
                 $result[$x]['zona'] = thunderToUtf8(trim($result[$x]['zona']));
                 $result[$x]['alias'] = thunderToUtf8(trim($result[$x]['alias']));
                 $result[$x]['materia'] = thunderToUtf8(trim($result[$x]['materia']));
-                $result[$x]['temp'] = $result[$x]['temp'] - 1.5;
-                $result[$x]['hum'] = $result[$x]['hum'] - 1.5;
+                $result[$x]['temp'] = $result[$x]['temp'] == 0.0 ? 0.0 : $result[$x]['temp'] - 1.5;
+                $result[$x]['hum'] = $result[$x]['hum'] == 0.0 ? 0.0 : $result[$x]['hum'] - 1.5;
             }
 
             ReturnEvent::returnResponse(0, "Datos obtenidos con exito", $result);
