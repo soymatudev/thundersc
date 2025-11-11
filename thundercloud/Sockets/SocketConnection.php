e195e2fe354d5e403b61ae7e364d1849fb796e16
juan <jmaturana@pcz.com.mx>
2025-11-11 11:37:59 -0600

    Eventos para sensores

--- thundercloud/Sockets/SocketConnection.php
+++ thundercloud/Sockets/SocketConnection.php
@@ -27,10 +27,10 @@ class SocketConnection
     {
         $this->conn = null;
         $this->thunderlog = new Log(null, $uu);
-        //$this->host = '148.230.206.170';
+        $this->host = '148.230.206.170';
         $this->port = $port ?? 1085;
-        $this->host = '127.0.0.1';
-        $this->port = 3000;
+        /* $this->host = '127.0.0.1';
+        $this->port = 3000; */
     }
 
     function socketCreate()
