4598f173a578db4c4d2e5a33103a40dc1040d05d
juan <jmaturana@pcz.com.mx>
2026-01-27 11:03:25 -0600

    Feat: Socket

--- api/src/features/sensores/sensores.service.js
+++ api/src/features/sensores/sensores.service.js
@@ -105,13 +105,14 @@ exports.getDashboardStatus = async (userId) => {
         const todayStart = dayjs().startOf('day').toDate();
         let ultimaLectura = null;
 
+        console.log(todayStart, absoluteLast.fecha_hora);
         if (absoluteLast) {
             // Aplicar corrección de zona horaria manual solicitada por el usuario
             const fecha = absoluteLast.fecha_hora;
             absoluteLast.fecha_hora = dayjs(dayjs(fecha).toString().split(' GMT')[0]).format('YYYY-MM-DD HH:mm:ss');
 
             // Solo consideramos la lectura como "actual" si es de hoy
-            console.log(todayStart, absoluteLast.fecha_hora);
+            
             if (absoluteLast.fecha_hora >= todayStart) {
                 ultimaLectura = absoluteLast;
             }
