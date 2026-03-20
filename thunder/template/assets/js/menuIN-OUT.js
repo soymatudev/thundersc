059260aea6f8f710408ebde738e073ba048bc725
juan <jmaturana@pcz.com.mx>
2026-03-20 09:19:48 -0600

    Chore: Delete old vesion with php

--- thunder/template/assets/js/menuIN-OUT.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async function() {
-    let username = true;
-
-    await $("#menuIN").on("click", async function() {
-        username ?  $("#title-user").hide() : $("#title-user").show();
-        username = !username;
-    })
-
-});
--- thundercloud/Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists('nextToUtf8')){
-    function thunderToUtf8($cadena){
-        $cadena=mb_convert_encoding($cadena,"UTF-8",mb_detect_encoding($cadena,array("UTF-8","ISO-8859-1"),true));       
-        return $cadena;
-    }
-}
